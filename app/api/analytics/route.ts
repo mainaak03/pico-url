935afc69bb7f6b1aa236bcc5c41f28693dc2b6ff
Mainaak Chakraborty <mainaakch@gmail.com>
2024-10-16 21:46:18 +0530

    fix dynamic routes error, update packages

--- app/api/analytics/route.ts
+++ app/api/analytics/route.ts
@@ -4,10 +4,9 @@ import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 
 export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const idStrings = searchParams.getAll('ids');
   try {
-    const { searchParams } = new URL(req.url);
-    const idStrings = searchParams.getAll('ids');
-
     const decoded_ids = idStrings.map((id) => BigInt(id));
 
     const analytics = await prisma.url.findMany({
